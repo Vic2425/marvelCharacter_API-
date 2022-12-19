e4ea1763d2aa99047029d3c93dbd0a491fcb13b1
Vic <vicjs612@gmail.com>
2022-12-19 17:36:50 -0600

    add nother pokemon fetch, html, css

--- main.js
+++ main.js
@@ -1,56 +1,83 @@
 
-// let pokemonOne;
-// let pokemonTwo;
-
 // window.onload = () => {
 //     getPokemon();
 // }
 
-// const getPokemon = (id) => {
-//     fetch(`https://pokeapi.co/api/v2/pokemon/${id}/`)
-//      .then(res => res.json())
-//      .then(arr => {
-//         pokemonOne = arr
-//      });
-    
-// }
-// getPokemon(5)
-
-
 const getPokemonOne = (e) => {
-    const name = document.querySelector('#pokemonName').value;
-    const pokemonName = name.toLowerCase();
+  const pokeId = document.querySelector('#pokeOneId').value;
 
-    fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`)
-     .then(res => res.json())
-     .then((data) => {
-        document.querySelector(".pokemonBox").innerHTML = `
-        <div>
-          <img
-            src="${data.sprites.other["official-artwork"].front_default}"
-            alt="Pokemon name"
-          />
-        </div>
-        <div class="pokemonInfos">
-          <h1>${capitalizeFirstLetter(data.name)}</h3>
-          <p>Weight: ${data.weight}</p>
-        </div>`;
-     })
-     .catch((err) => {
-        document.querySelector(".pokemonBox").innerHTML = `
-        <h4>Pokemon not found 😞</h4>
-        `;
-        console.log("Pokemon not found", err);
-      });
-  
-    e.preventDefault();
+  fetch(`https://pokeapi.co/api/v2/pokemon/${pokeId}`)
+   .then(res => res.json())
+   .then((data1) => {
+      document.querySelector(".pokemonOneBox").innerHTML = `
+      <div>
+        <img
+          src="${data1.sprites.other["official-artwork"].front_default}"
+          alt="Pokemon name" />
+      </div>
+      <div class="infoOne">
+        <h1>${capFirstLetter(data1.name)}</h3>
+        <p>Weight: ${data1.weight}</p>
+        <p>Base Experience: ${data1.base_experience}</p>          
+        <p>stat: ${data1.stats[1].base_stat}</p>          
+      </div>`;
+   })
+   .catch((err) => {
+      document.querySelector(".pokemonOneBox").innerHTML = `
+      <h4>Pokemon not found 😞</h4>
+      `;
+      console.log("Pokemon not found", err);
+    });
+  e.preventDefault();
 }
-document.querySelector("#search").addEventListener("click", getPokemonOne);
+document.querySelector("#searchOne").addEventListener("click", getPokemonOne);
 
-function capitalizeFirstLetter(string) {
-    return string.charAt(0).toUpperCase() + string.slice(1);
-  };
 
-function lowerCaseName(string) {
-    return string.toLowerCase();
-  }
+const getPokemonTwo = (e) => {
+  const pokeId = document.querySelector('#pokeTwoId').value;
+
+  fetch(`https://pokeapi.co/api/v2/pokemon/${pokeId}`)
+   .then(res => res.json())
+   .then((data2) => {
+      document.querySelector(".pokemonTwoBox").innerHTML = `
+      <div>
+        <img
+          src="${data2.sprites.other["official-artwork"].front_default}"
+          alt="Pokemon name" />
+      </div>
+      <div class="infoTwo">
+        <h1>${capFirstLetter(data2.name)}</h3>
+        <p>Weight: ${data2.weight}</p>
+        <p>Base Experience: ${data2.base_experience}</p>          
+        <p>stat: ${data2.stats[1].base_stat}</p>          
+      </div>`;
+   })
+   .catch((err) => {
+      document.querySelector(".pokemonTwoBox").innerHTML = `
+      <h4>Pokemon not found 😞</h4>
+      `;
+      console.log("Pokemon not found", err);
+    });
+  e.preventDefault();
+}
+document.querySelector("#searchTwo").addEventListener("click", getPokemonTwo);
+
+function capFirstLetter(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+};
+
+const pokeBattle = () => {
+let stat1 = `${data1.stats[1].base_stat}`
+let stat2 = `${data2.stats[1].base_stat}`
+const btn = document.getElementById('btn');
+btn.addEventListener('click', () => {
+   if(stat1 > stat2) {
+     console.log('PokeOne is a winner!')
+   } else if(stat1 < stat2) {
+       console.log('PokeTwo is a winner!')
+   } else if( stat1 = stat2) {
+     console.log('It\'s a tie!')
+   }
+})
+}
+
